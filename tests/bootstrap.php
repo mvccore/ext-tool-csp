04a58d1ae8cca1911b8f3c57d82193a9656574f4
Tom Flídr <tomflidr@gmail.com>
2022-06-20 19:14:23 +0200

    tests improvements

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,8 @@ include_once(__DIR__ . '/../vendor/autoload.php');
 
 if (php_sapi_name() !== 'cli') {
 	\Tracy\Debugger::enable(\Tracy\Debugger::DEVELOPMENT);
-	\Tracy\Debugger::$productionMode = false;	/** @var int  how long strings display by dump() */
+	\Tracy\Debugger::$productionMode = false;
+	/** @var int  how long strings display by dump() */
 	\Tracy\Debugger::$maxLength = 1024*1024;
 	\Tracy\Debugger::$showLocation = true;
 	
